e31aaf2a43abab5b7fa4ea48f9f6307619ccd666
Aleksi Suutarinen <aleksi.suutarinen@iki.fi>
2015-08-21 15:02:10 +0100

    Moving to Github

--- omnifit/__init__.py
+++ omnifit/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-This is a package for doing ice spectroscopy fitting that relies on astropy
+This is a package for doing ice spectroscopy fitting of interstellar ices
 """
 
 # Affiliated packages may add whatever they like to this file, but
