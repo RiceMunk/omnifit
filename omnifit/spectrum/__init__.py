909ed9dc42c9b789a40af139d934ccc12dac7178
Aleksi Suutarinen <aleksi.suutarinen@gmail.com>
2019-08-06 18:41:04 +0300

    pep8 compliance: spectrum maybe fixed but still compliant now

--- omnifit/spectrum/__init__.py
+++ omnifit/spectrum/__init__.py
@@ -1,1 +1,3 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+from .spectrum import BaseSpectrum, AbsorptionSpectrum, CDESpectrum
+__all__ = ['BaseSpectrum', 'AbsorptionSpectrum', 'CDESpectrum']
