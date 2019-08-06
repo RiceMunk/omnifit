6742122811e7063ffc157f3a84d8cd9cbea20229
Aleksi Suutarinen <aleksi.suutarinen@gmail.com>
2019-08-06 22:06:46 +0300

    pep8 compliance: functions and fitting now compliant, maybe

--- omnifit/fitter/__init__.py
+++ omnifit/fitter/__init__.py
@@ -1,8 +1,3 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-try:
-    # Not guaranteed available at setup time
-    from .fitter import Fitter
-except ImportError:
-    if not _ASTROPY_SETUP_:
-        raise
+from .fitter import Fitter
+__all__ = ['Fitter']
